0798330250656a33707378f442ca4a28068ccd06
UrielRmz2311 <125678140+UrielRmz2311@users.noreply.github.com>
2023-06-02 17:20:47 -0600

    Se creo contenido de pestaña Alta de bebidas

--- AlmacenBebidas/ventana.py
+++ AlmacenBebidas/ventana.py
@@ -14,8 +14,36 @@ baja=ttk.Frame(panel)
 consulta=ttk.Frame(panel)
 actualizar=ttk.Frame(panel)
 
+# Metodos ---------------------------------------------------------------------
+
+# Alta de Bebidas -------------------------------------------------------------
+titulo= Label(alta,text="Alta de Bebidas", fg="Blue", font=("Arial",18)).pack()
+
+varBe= tk.StringVar()
+iblBe= Label(alta, text="Nombre Bebida: ").pack()
+txtBe= Entry(alta, textvariable=varBe).pack()
+
+iblmClas= Label(alta, text= "Clasificación: ").pack()
+varClas = ("Energetica", "Azucarada", "Agua","Alcoholica","Soda","Otra")
+combo = ttk.Combobox(alta, values=varClas)
+combo.pack()
+
+varmar= tk.StringVar()
+iblmar= Label(alta, text="Marca: ").pack()
+txtmar= Entry(alta, textvariable=varmar).pack()
+
+varPre= tk.StringVar()
+iblPre= Label(alta, text="Precio: ").pack()
+txtPre= Entry(alta, textvariable=varPre).pack()
+
+btnGuardar= Button(alta, text="Registrar Bebida").pack()
+# Baja de Bebidas -------------------------------------------------------------
 
 
+# Consulta de Bebidas ---------------------------------------------------------
+
+
+# Actualizar Bebidas ----------------------------------------------------------
 
 panel.add(alta,text="Alta de Bebidas")
 panel.add(baja,text="Baja de Bebidas")
