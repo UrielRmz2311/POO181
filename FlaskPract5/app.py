a7ae3f9d5a2911fef20405e914e2c00ceb24fdcd
UrielRmz2311 <125678140+UrielRmz2311@users.noreply.github.com>
2023-06-14 17:11:18 -0600

    Se crearon nuevas rutas, y la conexion con la base de datos junto con la misma

--- FlaskPract5/app.py
+++ FlaskPract5/app.py
@@ -1,9 +1,32 @@
+# Importacion de framework -------------------------------------------
 from flask import Flask
-app= Flask(__name__)
+# Importacion de MySQL con FLASK
+from flask_mysqldb import MySQL 
 
+# Inicialización del APP ó servidor ----------------------------------
+app= Flask(__name__)
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='rt' # Usuario de MySQL 
+app.config['MYSQL_PASSWORD']=''  # Contraseña MySQL
+app.config['MYSQL_DB']='dbflask'  # Nombre de la base de datos
+mysql= MySQL(app)
+
+# Declaración de las rutas hhtp://localhost:5000 ---------------------
+
+  # Ruta principal -------------
 @app.route('/')
 def index():
     return "Hola Mundo FLASK"
 
+@app.route('/guardar')
+def guardar():
+    return "Se guardo en la BD"
+
+@app.route('/eliminar')
+def eliminar():
+    return "Se elimino en la BD"
+
+# Ejecución de Servidor en el Puerto 5000 ---------------------------- 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000,debug=True)
+    
