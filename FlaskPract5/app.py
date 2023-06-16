56b8680125d75f3bd4ab971f66ba5675eac5e4a8
UrielRmz2311 <125678140+UrielRmz2311@users.noreply.github.com>
2023-06-16 17:07:08 -0600

    Practica 6: Preparamos vistas y enviamos datos del formulario a la ruta

--- FlaskPract5/app.py
+++ FlaskPract5/app.py
@@ -1,12 +1,14 @@
 # Importacion de framework -------------------------------------------
-from flask import Flask
+from flask import Flask,render_template, request
 # Importacion de MySQL con FLASK
-from flask_mysqldb import MySQL 
+from flask_mysqldb import MySQL
 
 # Inicialización del APP ó servidor ----------------------------------
 app= Flask(__name__)
+
+# Conexion de la base de datos ---------------------------------------
 app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='rt' # Usuario de MySQL 
+app.config['MYSQL_USER']='root' # Usuario de MySQL 
 app.config['MYSQL_PASSWORD']=''  # Contraseña MySQL
 app.config['MYSQL_DB']='dbflask'  # Nombre de la base de datos
 mysql= MySQL(app)
@@ -16,11 +18,17 @@ mysql= MySQL(app)
   # Ruta principal -------------
 @app.route('/')
 def index():
-    return "Hola Mundo FLASK"
+    return render_template('index.html')
 
-@app.route('/guardar')
+# ruta http:localhost:500/guardar tipo POST para Insert
+@app.route('/guardar', methods=['POST'] )
 def guardar():
-    return "Se guardo en la BD"
+    if request.method == 'POST':
+        titulo= request.form['txtTitulo']
+        artista= request.form['txtArtista']
+        anio= request.form['txtAnio']
+        print(titulo,artista,anio)
+    return "Los datos llegaron Amigo ;"
 
 @app.route('/eliminar')
 def eliminar():
